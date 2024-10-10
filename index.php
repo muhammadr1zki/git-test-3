4082dae02a37b7777cbf69af7664e1030db4e50c
muhammadr1zki <muhammadrizkiam02@gmail.com>
2024-10-10 15:44:05 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor perspiciatis fugit modi exercitationem dolorum id aliquam sed necessitatibus recusandae numquam vitae tenetur sit qui molestias nostrum sunt ratione, eligendi aliquid.</p>
 </body>
 </html>
